a9e86a60d51f4f950bc9562d0447167499d7273c
izaias filho <izs.af@hotmail.com>
2022-04-19 07:35:28 -0300

    proyecto rh-trabajadores creado

---
+++ hr-trabajadores/src/main/java/com/izs/rhtrabajadores/HrTrabajadoresApplication.java
@@ -0,0 +1,13 @@
+package com.izs.rhtrabajadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrTrabajadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrTrabajadoresApplication.class, args);
+	}
+
+}

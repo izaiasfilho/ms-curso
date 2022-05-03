ac76cb69a93ee21f8c789db717a716bea48e0d22
izaias filho <izs.af@hotmail.com>
2022-05-03 07:20:13 -0300

    proyecto de nomina creado

---
+++ hr-nomina/src/test/java/com/izs/rhnomina/HrNominaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izs.rhnomina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrNominaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

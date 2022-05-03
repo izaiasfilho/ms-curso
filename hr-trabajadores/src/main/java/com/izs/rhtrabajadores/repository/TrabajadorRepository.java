61bbf72816c0866652d7adb12718c803d25a91fd
izaias filho <izs.af@hotmail.com>
2022-05-03 07:12:53 -0300

    criaçao do nomina

---
+++ hr-trabajadores/src/main/java/com/izs/rhtrabajadores/repository/TrabajadorRepository.java
@@ -0,0 +1,9 @@
+package com.izs.rhtrabajadores.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.izs.rhtrabajadores.entities.Trabajador;
+
+public interface TrabajadorRepository extends JpaRepository<Trabajador, Long>{
+
+}

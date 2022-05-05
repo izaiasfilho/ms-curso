8eabadd02bdf64df826a52d141e6822b5516cba1
izaias filho <izs.af@hotmail.com>
2022-05-05 07:41:26 -0300

    RestTamplate

---
+++ hr-nomina/src/main/java/com/izs/rhnomina/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.izs.rhnomina.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public RestTemplate restTempllate() {
+		return new RestTemplate();
+	}
+}
--- hr-nomina/src/main/java/com/izs/rhnomina/services/PaymentServices.java
+++ hr-nomina/src/main/java/com/izs/rhnomina/services/PaymentServices.java
@@ -1,14 +1,33 @@
 package com.izs.rhnomina.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents.UriTemplateVariables;
 
 import com.izs.rhnomina.entities.Payment;
+import com.izs.rhnomina.entities.Trabajador;
 
 @Service
 public class PaymentServices {
+	
+	@Value("${hr-trabajador.host}")
+	private String trabajadorHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		
+		Map<String,String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		Trabajador trabajador = restTemplate.getForObject(trabajadorHost + "/trabajador/{id}",Trabajador.class,uriVariables);
+		return new Payment(trabajador.getName(), trabajador.getDailyIncome(), days);
 	}
 	
 }

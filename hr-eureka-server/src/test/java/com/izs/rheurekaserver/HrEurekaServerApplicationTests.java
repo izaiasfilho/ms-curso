588a5ee6097f8a1ced3f110205bbf4d6556b839e
izaias filho <izs.af@hotmail.com>
2022-05-09 07:54:16 -0300

    creat eureka-server

---
+++ hr-eureka-server/src/test/java/com/izs/rheurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izs.rheurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
